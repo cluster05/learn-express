e46ff105e7e65e01d4b3624e115ccdf83cac0e75
cluster05 <ajayakumbhar1234@gmail.com>
2021-01-29 12:49:23 +0530

    create fake auth with localstorage

--- server.js
+++ server.js
@@ -6,9 +6,33 @@ const PORT = 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+const FAKE_DB = [
+    { username: 'user1', email: 'user1@mail.com', password: 'password' },
+    { username: 'user2', email: 'user2@mail.com', password: 'password' },
+    { username: 'user3', email: 'user3@mail.com', password: 'password' }
+];
+
 app.post('/auth/register', (req, res) => {
-    res.send(req.body)
-})
+    const { username, email, password } = req.body;
+    FAKE_DB.push({ username, email, password });
+    res.send(FAKE_DB)
+});
+
+app.post('/auth/login', (req, res) => {
+    const { email, password } = req.body;
+    FAKE_DB.forEach(user => {
+        if (user.email === email) {
+            if (user.password === password) {
+                res.send({ access_token: '123456789876543212345678-876543210' })
+            } else {
+                res.send({ message: 'Invalid Password' })
+            }
+        } else {
+            res.send({ message: 'Invalid Credentails' })
+        }
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
