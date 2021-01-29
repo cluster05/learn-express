ec7b048ff0e1bc6d2518464cfc1c9f994654f85e
cluster05 <ajayakumbhar1234@gmail.com>
2021-01-29 11:24:16 +0530

    added router for clean clode

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const app = express();
+const birdRouter = require('./bird');
 const PORT = 3000
 
 app.use('/public', express.static('/public'))
 
-app.get('/download', (req, res) => {
-    res.download('public/static/hello.txt')
-})
-
-app.get('/json/:id', (req, res) => {
-    res.json({ id: req.params.id });
-    res.redirect('/download')
-})
-app.get('/render', (req, res) => {
-    res.render('public/static/index.html')
-})
+app.use('/bird', birdRouter);
 
 
 app.listen(PORT, () => {
